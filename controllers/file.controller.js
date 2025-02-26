d7df49756db45e2c4c8bbf4a6b91fe19fd445954
Phuoc Nhan <michaeltrannhan@gmail.com>
2025-02-26 16:09:08 +0700

    fix: upload bug

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -7,7 +7,14 @@ const multer = require("multer");
 const { Readable } = require("stream");
 
 // Configure multer storage
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 const upload = multer({
   storage,
   limits: {
